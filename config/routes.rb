6178128a8873b9c56a0a6a84c633d2a939c7c611
Tom-Eric Gerritsen <tomeric@eet.nu>
2022-10-17 03:30:20 +0200

    Make the gem mountable in rails with `mount Operator::Engine => '/operator'`

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.prepend do
-  post "operator/:queue" => "operator/messages#create", :as => :process_message
+if Operator::Engine.respond_to?(:routes)
+  Operator::Engine.routes.draw do
+    post ':queue' => 'operator/messages#create', :as => :process_message
+  end
+else
+  Rails.application.routes.prepend do
+    post 'operator/:queue' => 'operator/messages#create', :as => :process_message
+  end
 end
